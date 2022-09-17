bcdb99c53a731e748f3c3da0afac53e71aaebc60
Robby Pambudi <5025211042@mhs.its.ac.id>
2022-09-17 14:53:42 +0700

    feat: add constant

---
+++ src/lib/constant.jsx
@@ -0,0 +1,76 @@
+export const Jenis_Transaksi = [
+  {
+    name: 'Pengeluaran',
+    value: 'pengeluaran',
+  },
+  {
+    name: 'General',
+    value: 'general',
+  },
+  {
+    name: 'Pemasukan',
+    value: 'pemasukan',
+  },
+  {
+    name: 'Hutang',
+    value: 'hutang',
+  },
+  {
+    name: 'Piutang',
+    value: 'piutang',
+  },
+  {
+    name: 'Tanam Modal',
+    value: 'tanam-modal',
+  },
+  {
+    name: 'Tarik Modal',
+    value: 'tarik-modal',
+  },
+  {
+    name: 'Transfer Uang',
+    value: 'transfer-uang',
+  },
+  {
+    name: 'Pemasukan Sebagai Piutang',
+    value: 'pemasukan-sebagai-piutang',
+  },
+  {
+    name: 'Pemasukan Sebagai Hutang',
+    value: 'pemasukan-sebagai-hutang',
+  },
+];
+
+export const Jenis_Debit = [
+  {
+    name: 'Beban Pokok Pendapatan (5-50000)',
+    value: '5-50000',
+  },
+  {
+    name: 'Diskon Pembelian (5-50100)',
+    value: '5-50100',
+  },
+  {
+    name: 'Pengembalian Pembelian  (5-50200)',
+    value: '5-50200',
+  },
+];
+
+export const Jenis_Kredit = [
+  {
+    name: 'Kas (1-10001)',
+    value: '1-10001',
+  },
+  {
+    name: 'Rekening Bank (1-10002)',
+    value: '1-10002',
+  },
+  {
+    name: 'Bank Mandiri (1-10003)',
+    value: '1-10003',
+  },
+  {
+    name: 'Bank Negara Indonesia (BNI) (1-10004)',
+    value: '1-10004',
+  },
+];
