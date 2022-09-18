a06082baf69088dc708da0ebf68e176af55e7d18
Robby Pambudi <5025211042@mhs.its.ac.id>
2022-09-18 13:45:53 +0700

    feat: add invoice route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,10 @@
 import Login from '../pages/Auth/Login';
 import Dashboard from '../pages/Dashboard';
-import Transaksi from '../pages/Transaksi';
+import InvoiceBarang from '../pages/Invoice/InvoiceBarang';
+import JurnalUmum from '../pages/Laporan/JurnalUmum';
+import NeracaSaldo from '../pages/Laporan/NeracaSaldo';
+import TransaksiBarang from '../pages/Transaksi/TransaksiBarang';
+import TransaksiUmum from '../pages/Transaksi/TransaksiUmum';
 import AuthRoute from './AuthRoute';
 import GuestRoute from './GuestRoute';
 
@@ -16,10 +20,37 @@ const routes = [
     exact: true,
   },
   {
-    path: '/transaksi',
+    path: '/laporan/neraca_saldo',
     element: (
       <AuthRoute>
-        <Transaksi />
+        <NeracaSaldo />
+      </AuthRoute>
+    ),
+    visible: true,
+  },
+  {
+    path: '/transaksi/umum',
+    element: (
+      <AuthRoute>
+        <TransaksiUmum />
+      </AuthRoute>
+    ),
+    visible: true,
+  },
+  {
+    path: '/transaksi/barang',
+    element: (
+      <AuthRoute>
+        <TransaksiBarang />
+      </AuthRoute>
+    ),
+    visible: true,
+  },
+  {
+    path: '/laporan/jurnal_umum',
+    element: (
+      <AuthRoute>
+        <JurnalUmum />
       </AuthRoute>
     ),
     visible: true,
@@ -30,6 +61,17 @@ const routes = [
       <GuestRoute>
         <Login />
       </GuestRoute>
+    ),
+    visible: true,
+  },
+
+  // Invoice Barang
+  {
+    path: '/invoice/barang/:id',
+    element: (
+      <AuthRoute>
+        <InvoiceBarang />
+      </AuthRoute>
     ),
     visible: true,
   },
