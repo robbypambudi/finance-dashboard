7eb25ae65d2265a457d3884c7298c953adae75af
Robby Pambudi <5025211042@mhs.its.ac.id>
2022-09-25 19:51:47 +0700

    feat: add faktur

--- src/pages/Transaksi/Barang/FakturPembelian.jsx
+++ src/pages/Transaksi/Barang/FakturPembelian.jsx
@@ -1,10 +1,71 @@
+import { createColumnHelper } from '@tanstack/react-table';
+import { FaEye, FaPrint, FaRegEdit } from 'react-icons/fa';
+import Table from '../../../components/Table';
 import DashboardShellAdmin from '../../../layouts/admin/DashboarsShellAdmin';
 
 export default function FakturPembelian() {
+  const DUMYDATA = [
+    {
+      'kode-transaksi': '10234',
+      tanggal: '12',
+      status: 'Selesai',
+    },
+    {
+      'kode-transaksi': '10235',
+      tanggal: '12',
+      status: 'Selesai',
+    },
+    {
+      'kode-transaksi': '10236',
+      tanggal: '12',
+      status: 'Selesai',
+    },
+  ];
+
+  const columnsHelper = createColumnHelper();
+
+  const ListFaktur = [
+    columnsHelper.accessor('kode-transaksi', {
+      cell: (info) => info.getValue(),
+      header: <span>Kode Transaksi</span>,
+    }),
+    columnsHelper.accessor('tanggal', {
+      cell: (info) => info.getValue(),
+      header: <span>Tanggal</span>,
+    }),
+
+    columnsHelper.accessor('status', {
+      cell: (info) => info.getValue(),
+      header: <span>Status</span>,
+    }),
+    columnsHelper.accessor('kode-transaksi', {
+      cell: (info) => {
+        return (
+          <>
+            <div className='flex flex-col items-center justify-center gap-y-3'>
+              <FaEye />
+              <FaRegEdit />
+              <FaPrint />
+            </div>
+          </>
+        );
+      },
+      header: <span>Aksi</span>,
+    }),
+  ];
+
   return (
     <>
       <DashboardShellAdmin>
-        <h2></h2>
+        <div className='p-10'>
+          <h2 className='text-center text-3xl font-primary'>
+            Faktur Pembelian
+          </h2>
+
+          <div className='bg-white rounded p-4'>
+            <Table data={DUMYDATA} columns={ListFaktur} />
+          </div>
+        </div>
       </DashboardShellAdmin>
     </>
   );
